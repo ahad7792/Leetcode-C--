7af30e93160c3f04d58506523b60a8ef65dd4afa
ahad7792 <ahad7792@gmail.com>
2022-09-21 22:10:29 +0600

    commit

--- FirstUniqueCharInStrg.cpp
+++ FirstUniqueCharInStrg.cpp
@@ -1,15 +1,14 @@
 #include<bits/stdc++.h>
-#include<iostream>
-using std::string
+using namespace std;
 
 int firstUniqChar(string s) {
     map<string, int> m;
     for(int i = 0; i < s.size(); i++){
-        m[s[i]] += 1;
+        m["s[i]"] += 1;
     }
 
     for(int i = 0; i < s.size(); i++){
-        if(m[s[i]] == 1) return i; //returning the index of first non repeating char
+        if(m["s[i]"] == 1) return i; //returning the index of first non repeating char
     }
 
     return -1;
