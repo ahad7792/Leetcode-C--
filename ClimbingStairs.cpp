d86e0f4810c40f9d0990a5b96bab084962aae6dc
ahad7792 <ahad7792@gmail.com>
2022-10-05 12:33:06 +0600

    commit

---
+++ ClimbingStairs.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int helper(int n, vector<int> &dp)
+{
+        if(n==0) return 1;
+        if(n<0) return 0;
+        if(dp[n]!=0)
+            return dp[n];
+        return dp[n] = helper(n-1,dp) + helper(n-2,dp);
+}
+
+int climbStairs(int n) {
+        vector<int> dp(n+1,0);
+        int count = helper(n,dp);
+        return count;
+}
