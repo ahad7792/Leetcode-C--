f469c2a7211cb5c259f37f6dfd4a3b232470c7b0
ahad7792 <ahad7792@gmail.com>
2022-10-20 19:40:33 +0600

    commit

--- RomanToInteger.cpp
+++ RomanToInteger.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+using std::map;
 
 int romanToInt(string s)
 {
@@ -42,4 +43,33 @@ int romanToInt(string s)
         prev = num;
     }
     return answer;
+}
+
+map<char, int> romanToIntMap{
+    {'I', 1},
+    {'V', 5},
+    {'X', 10},
+    {'L', 50},
+    {'C', 100},
+    {'D', 500},
+    {'M', 1000}};
+
+int romanToIntRe(string s)
+{ // use recursion
+    // base case:
+    if (s.size() == 1)
+    {
+        return romanToIntMap[s[0]];
+    }
+    // recursion
+    int l = romanToIntMap[s[0]];
+    int r = romanToIntMap[s[1]];
+    if (l < r)
+    {
+        return romanToIntRe(s.substr(1)) - l;
+    }
+    else
+    {
+        return romanToIntRe(s.substr(1)) + l;
+    }
 }
